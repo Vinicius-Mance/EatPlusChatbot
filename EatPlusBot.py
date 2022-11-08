f74aee97cf7ff49057ea4426f79c8a120d3cb568
Vinicius-Mance <vinimanceprof@gmail.com>
2022-11-08 01:03:30 -0300

    update: cadastro dividido em seções corretamente

--- EatPlusBot.py
+++ EatPlusBot.py
@@ -1,10 +1,8 @@
-from symbol import if_stmt
-
 import requests
 import json
 import os
 from User import User
-
+import re
 
 class EatPlusBot:
 
@@ -12,11 +10,10 @@ class EatPlusBot:
 
         token = '5700849117:AAGyTAPHlWpF0IoTnogZ8aN-EU0QL_m1zwk'
         self.url = f'https://api.telegram.org/bot{token}/'
-        self.user = User
+        self.user = User()
 
     def Start(self):
 
-        respota2 = ""
         update_id = None
 
         while True:
@@ -31,104 +28,93 @@ class EatPlusBot:
                     update_id = info['update_id']
                     message = str(info["message"]["text"])
                     chat_id = info["message"]["from"]["id"]
-                    print(chat_id)
                     first_message = int(info["message"]["message_id"]) == 1
 
-                    answer = self.create_answer(message, first_message)
-                    respota2 = answer
-
-                    if answer == "Qual o seu nome?":
-                        respota2 = answer
-                        self.response(respota2, chat_id)
-                        motor = True
-                        while motor:
-
-                            update = self.read_new_messages(update_id)
-                            data = update["result"]
-
-                            if data:
-
-                                for info in data:
-                                    update_id = info['update_id']
-                                    message = str(info["message"]["text"])
-                                    chat_id = info["message"]["from"]["id"]
-                                    print(chat_id)
-                                    first_message = int(info["message"]["message_id"]) == 1
-
-                                    answer = self.metodoDois(message, first_message)
-                                    respota2 = answer
-
-                                    if answer == "Cadastro finalizado com sucesso!":
-                                        respota2 = answer
-                                        self.response(respota2, chat_id)
-                                        motor = False
-
-                                    self.response(respota2, chat_id)
-
-                    self.response(respota2, chat_id)
+                    update = self.read_new_messages(update_id)
+                    answer = self.create_answer(message, first_message,chat_id)
+                    self.response(answer, chat_id)
 
     def read_new_messages(self, update_id):
 
-        link_req = f'{self.url}getUpdates?timeout=5'
+        link_req = f'{self.url}getUpdates?timeout=1'
 
         if update_id:
             link_req = f'{link_req}&offset={update_id + 1}'
-
         result = requests.get(link_req)
 
         return json.loads(result.content)
 
-    def create_answer(self, message, first_message):
+    def create_answer(self, message, first_message, chat_id):
 
         print('client message: ' + str(message))
 
-        if first_message == True or message.lower() in ('oi', 'Oi', 'oi.', 'Oi.', 'oi!', 'Oi!'):
-            return f'Eu me chamo Rafaela.{os.linesep}Vou preencher sua ficha pra você, então preciso que você me fale as informações que te pedir.{os.linesep}Podemos começar, Sim ou não?'
+        condition_no = message == message in ('não', 'n', 'Não', 'não.', 'Não.', 'nao', 'Nao', 'Nao.', 'nao.')
+        condition_yes = message == message in ('sim', 's', 'Sim', 'sim.', 'Sim.')
+        regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
-        if message == message in ('sim', 's', 'Sim', 'sim.', 'Sim.'):
-            return "Qual o seu nome?"
+        if self.user.id == '':
+            if first_message == True or message in ('oi', 'Oi', 'oi.', 'Oi.', 'oi!', 'Oi!') or '/start':
+                self.user.id = chat_id
+                return f'Eu me chamo Rafaela.{os.linesep}Vou preencher sua ficha pra você, então preciso que você me fale as informações que te pedir.{os.linesep}Podemos começar, Sim ou não?'
 
-        if message == message in "1":
-            return "Agora qual seu E-mail?"
+            else:
+                return 'Se quiser falar comigo, só dizer "oi"!'
 
-        if message == message in "2":
-            return "Por fim digite sua Senha"
+        if self.user.name == '':
+            if message == message in ('sim', 's', 'Sim', 'sim.', 'Sim.','continuar','Continuar'):
+                self.user.name = 'placeholder'
+                return "Qual o seu nome?"
 
-        if message == message in "3":
+            elif condition_no:
+                return 'Me avise quando estiver pronto(a)! Só dizer "continuar".'
 
-            return "Cadastro finalizado com sucesso!"
+            else:
+                return f"Poderia verificar sua resposta?"
 
-        elif message == message in ('não', 'n', 'Não', 'não.', 'Não.', 'nao', 'Nao', 'Nao.', 'nao.'):
-            return 'Quando estiver pronto(a), me diga "oi" que eu venho!'
+        if self.user.name == 'placeholder' and self.user.email == '':
+            self.user.name = message
+            return f"Seu nome será {self.user.name}. Tem certeza? sim, ou não?"
 
-        else:
-            return 'Se quiser falar comigo, só dizer "oi"!'
+        if self.user.name != 'placeholder' and self.user.email == '':
+            if condition_yes:
+                self.user.email = 'place@holder.com'
+                return "Qual o seu email?"
+            elif condition_no:
+                self.user.name = 'placeholder'
+                return "Reescreva seu nome"
+            else:
+                return f"Seu nome será {self.user.name}. Tem certeza? sim, ou não?"
 
-    def metodoDois(self, message, first_message):
-        print('client message: ' + str(message))
+        if self.user.email == 'place@holder.com' and self.user.name != 'placeholder':
+            if re.fullmatch(regex, message):
+                self.user.email = message
+                return f"Seu email será {self.user.email}. Tem certeza? sim ou não?"
+            else:
+                return "Escreva um email válido"
 
-        if first_message == True or message.lower() in ('oi', 'Oi', 'oi.', 'Oi.', 'oi!', 'Oi!'):
-            return f'Eu me chamo Rafaela.{os.linesep}Vou preencher sua ficha pra você, então preciso que você me fale as informações que te pedir.{os.linesep}Podemos começar, Sim ou não?'
+        if self.user.email != 'place@holder.com':
+            if condition_yes:
+                return "Qual será sua senha? (mínimo de 6 caractéres)"
+            elif condition_no:
+                self.user.email = 'place@holder.com'
+                return "Reescreva seu email"
 
-        if message == message in ('sim', 's', 'Sim', 'sim.', 'Sim.'):
-            return "Qual o seu nome?"
+        if self.user.email != 'place@holder.com' and self.user.name != 'placeholder' and self.user.password == '':
+            if len(message) >= 6:
+                self.user.password = message
+                return f"Sua senha será {self.user.password}. Tem certeza? sim ou não?"
+            else:
+                return "Escreva uma senha válida"
 
-        if message == message in "1":
-            return "Agora qual seu E-mail?"
+        if self.user.password != 'placeholder':
+            if condition_yes:
+                return "Qual será sua senha? (mínimo de 6 caractéres)"
+            elif condition_no:
+                self.user.password = 'placeholder'
+                return "Reescreva sua senha"
 
-        if message == message in "2":
-            return "Por fim digite sua Senha"
-
-        if message == message in "3":
-
-            return "Cadastro finalizado com sucesso!"
-
-        elif message == message in ('não', 'n', 'Não', 'não.', 'Não.', 'nao', 'Nao', 'Nao.', 'nao.'):
-            return 'Quando estiver pronto(a), me diga "oi" que eu venho!'
-
-        else:
-            return f'Digite 1 para confirmar seu Nome.{os.linesep}Digite 2 para confirmar seu E-mail.{os.linesep}Digite 3 para confirmar sua Senha.'
-
+        if self.user.email != 'place@holder.com' and self.user.name != 'placeholder' and self.user.password != 'placeholder':
+                return
     def response(self, answer, chat_id):
 
         link_req = f'{self.url}sendMessage?chat_id={chat_id}&text={answer}'
--- User.py
+++ User.py
@@ -1,6 +1,7 @@
 class User:
 
     def __init__(self):
+        self.id = ''
         self.name = ''
         self.email = ''
         self.password = ''
